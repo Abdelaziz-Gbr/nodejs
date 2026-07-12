0354a8f9e433c58c263f74032b227b7e9ab32d30
Abdelaziz-Gbr <abdelazizkhalid@outlook.com>
2026-07-12 18:28:00 +0300

    new version

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('hello hope i can reach u now.\n');
+  res.end('hello from the new version\n');
 });
 
 server.listen(PORT, () => {
