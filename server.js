f9f68ff1c62d90b510097fe42852dd76877e02c3
Abdelaziz-Gbr <abdelazizkhalid@outlook.com>
2026-07-17 20:27:47 +0300

    cer 1

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('hello from the new version\n');
+  res.end('version 1.0.0\n');
 });
 
 server.listen(PORT, () => {
